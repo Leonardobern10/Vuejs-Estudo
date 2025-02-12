e189ccccd2262a7037c1ff69187ac4a2f168ba9f
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-02-12 19:14:21 -0300

    new file:   vue8/.gitignore 	new file:   vue8/.vscode/extensions.json 	new file:   vue8/README.md 	new file:   vue8/index.html 	new file:   vue8/jsconfig.json 	new file:   vue8/package-lock.json 	new file:   vue8/package.json 	new file:   vue8/public/favicon.ico 	new file:   vue8/src/App.vue 	new file:   vue8/src/main.js 	new file:   vue8/vite.config.js 	new file:   vue9-df3/.gitignore 	new file:   vue9-df3/.vscode/extensions.json 	new file:   vue9-df3/README.md 	new file:   vue9-df3/index.html 	new file:   vue9-df3/jsconfig.json 	new file:   vue9-df3/package-lock.json 	new file:   vue9-df3/package.json 	new file:   vue9-df3/public/favicon.ico 	new file:   vue9-df3/src/App.vue 	new file:   vue9-df3/src/main.js 	new file:   vue9-df3/vite.config.js

---
+++ vue8/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from "vue";
+
+const mensagem = ref("");
+const aceito = ref(false);
+const escolha = ref("react");
+</script>
+
+<template>
+  <!--* O texto digitado no input está vinculado à variável mensagem -->
+  <div class="ex">
+    <input v-model="mensagem" placeholder="Digite Algo" />
+    <p>{{ mensagem }}</p>
+  </div>
+
+  <div class="ex">
+    <input type="checkbox" v-model="aceito" />Aceito os termos
+    <p>{{ aceito }}</p>
+  </div>
+  <div class="ex">
+    <input type="radio" v-model="escolha" value="vue" />vue
+    <input type="radio" v-model="escolha" value="react" /> react
+    <p>Escolha: {{ escolha }}</p>
+  </div>
+</template>
+
+<style scoped>
+.ex {
+  width: 100%;
+  height: fit-content;
+  border: 2px solid black;
+  padding: 1rem;
+}
+</style>
---
+++ vue9-df3/src/App.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref } from "vue";
+
+const nome = ref("");
+const idade = ref(null);
+const genero = ref("masculino");
+const aceito = ref(false);
+
+const produtoSelecionado = ref(null);
+const quantidade = ref(null);
+const produtos = [
+  { id: 1, nome: "Notebook", preco: 3000 },
+  { id: 2, nome: "Smartphone", preco: 2000 },
+  { id: 3, nome: "Fone de Ouvido", preco: 150 },
+];
+</script>
+<template>
+  <div id="div1">
+    <h2>Cadastro</h2>
+    <label>Nome:</label>
+    <input type="text" v-model.trim="nome" placeholder="Digite seu nome" />
+
+    <label>Idade:</label>
+    <input
+      type="number"
+      v-model.number="idade"
+      placeholder="Digite sua idade"
+    />
+
+    <label>Gênero:</label>
+    <label>
+      <input type="radio" v-model="genero" value="masculino" />
+      Masculino
+    </label>
+    <label>
+      <input type="radio" v-model="genero" value="feminino" />
+      Feminino
+    </label>
+
+    <label>
+      <input type="checkbox" v-model="aceito" />
+      Aceito os termos
+    </label>
+
+    <h3>Resumo do Cadastro</h3>
+    <p><strong>Nome:</strong> {{ nome }}</p>
+    <p><strong>Idade:</strong> {{ idade }}</p>
+    <p><strong>Gênero:</strong> {{ genero }}</p>
+    <p><strong>Aceitou os Termos?</strong> {{ aceito ? "Sim" : "Não" }}</p>
+  </div>
+
+  <div id="div2">
+    <label for="produtos">Selecione o produto</label>
+    <select v-model="produtoSelecionado" name="produtos" id="produtos">
+      <option v-for="prod in produtos" :key="prod.id" :value="prod">
+        {{ prod.nome }}
+      </option>
+    </select>
+    <label>Quantidade</label>
+    <input
+      type="number"
+      v-model.number="quantidade"
+      placeholder="Informe a quantidade"
+    />
+    <p>
+      O valor final será:
+      {{ produtoSelecionado ? produtoSelecionado.preco * quantidade : 0 }}
+    </p>
+  </div>
+</template>
